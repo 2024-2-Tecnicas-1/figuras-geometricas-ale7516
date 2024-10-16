f0a66d48cfc5eee295bb69abffa55b08ff2c18de
ale7516 <vanessaalejandrbarreramartin@gmail.com>
2024-10-15 19:48:17 -0500

    Se creo la clase circulo y super clase

---
+++ src/main/java/com/mycompany/figurasgeometricas/Circulo.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.figurasgeometricas;
+
+public class Circulo extends FiguraGeometrica {
+    private double radio;
+          
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this. radio=radio;
+    
+        
+    public void obtenerArea(){
+        double areaCirculo= 
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    
+
+        
+    }
+        
+    }
+
+    
+
